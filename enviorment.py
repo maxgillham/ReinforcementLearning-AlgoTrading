9ea70745185e090f4bec71b0a390535378a1f612
maxgillham <14mfg2@queensu.ca>
2018-11-02 17:55:52 -0400

    I can't decide on obsercation space

--- enviorment.py
+++ enviorment.py
@@ -1,6 +1,7 @@
 import gym
 
 from gym import spaces
+from utils import *
 
 class TradingEnv(gym.Env):
     def __init__(self, train_data, init_capital):
@@ -18,6 +19,13 @@ class TradingEnv(gym.Env):
         #actually just do buy sell hold for now
         self.action_space = spaces.Discrete(3*self.n_stocks)
         #observation space
-        capital_range = [[0, 2*init_capital]]
-        stock_return_rate_range = [[-5, 5]]
-        self.observation_space = spaces.MultiDiscrete(stock_return_rate_range + capital_range)
+        capital_range = 2*init_capital
+        stock_return_rate_range = get_max_and_min(self.stock_return_rate_history)[0]
+        #self.observation_space = spaces.Tuple((
+            #spaces.Discrete(stock_return_rate_range),
+            #spaces.Discrete(capital_range)))
+        #self.observation_space = spaces.MultiDiscrete(capital_range + stock_return_rate_range)
+        self.observation_space = spaces.Discrete(capital_range)
+
+        #self._seed()
+        #self._reset()
