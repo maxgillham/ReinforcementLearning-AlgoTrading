153c3ecb3e5349b2c19e9f981522fbe8f036e509
stephan.z <yuankangzhang01@gmail.com>
2018-11-04 19:26:04 -0500

    tried update function for Q table update
    Tried to implement an update function for Q table update caused by the env change. Left some places where env value is needed.
    
    Meanwhile, I commented out the Q table initialization function to adapt the new Q_table.py. In my implementation, I use pandas package for the table stracture initialization, which might have potential advantage for future work.

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import numpy as np
 
 from enviorment import TradingEnv
 from gym import spaces
+from Q_table import QLearningTable
 from utils import *
 
 episoides = 1000
-
+'''
 def initialize_Q():
     Q = {}
 
@@ -21,8 +22,32 @@ def initialize_Q():
             Q[state][action] = 0
 
     return Q
+'''
+def update():
+    #by default the training is set to be 100 episodes per training
+    for episode in range(100):
+        # initial observation
+        observation = env.reset()
 
+        while True:
+            # update env
+            # did not finish TradingEnv for fresh env yet
 
+            # RL choose action based on observation
+            action = Q.choose_action(str(observation))
+
+            # RL take action and get next observation and reward
+            # return next step observation_, reward from env 
+
+            # RL learn from this transition
+            Q.learn(str(observation), action, reward, str(observation_))
+
+            # swap observation
+            observation = observation_
+
+            # break while loop when end of this episode
+            if done:
+                break
 
 
 if __name__ == '__main__':
@@ -36,9 +61,9 @@ if __name__ == '__main__':
 
     #init Q table
     #Q = np.zeros((env.observation_space.n, env.action_space.n))
-
-    Q = initialize_Q()
-
+    #Q = initialize_Q()
+    Q = QLearningTable(actions=list(range(env.action_space.n)))
+    
     print(Q)
 
     #number of episoides
