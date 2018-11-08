98c3f79656278db6cb71e6664145985fb67c7c33
maxgillham <14mfg2@queensu.ca>
2018-11-07 20:54:15 -0500

    plotting updated

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from utils import *
 episodes = 100
 
 def update():
-    episode_num = []
     ending_cap = []
     #by default the training is set to be 100 episodes per training
     for episode in range(episodes):
@@ -38,8 +37,9 @@ def update():
             # break while loop when end of this episode
             if done:
                 print('Completed episoide...\nEnding episoide with: $', env.current_capital)
+                ending_cap.append(env.current_capital)
                 break
-    plt.scatter(episode_num, ending_cap, marker='.', c='k' )
+    plt.scatter(np.arange(episodes), ending_cap, marker='.', c='k' )
     plt.title('Capital Attained at Each Episode')
     plt.xlabel('Episode')
     plt.ylabel('Capital Attained')
@@ -49,12 +49,14 @@ def update():
 
 if __name__ == '__main__':
     train_data = round_return_rate(get_data())
+
+    #init trading env
     env = TradingEnv(train_data, init_capital=100)
 
     #init Q table
     Q = QLearningTable(actions=list(range(env.action_space.n)))
 
-    #print(Q.q_table)
-    #print(Q.choose_action(env._get_obs()))
-
+    #train method
     update()
+
+    print(Q.q_table)
