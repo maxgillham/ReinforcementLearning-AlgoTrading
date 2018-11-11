dd8330140d7db2f1f6407b1837e8e7a99b87a09e
Max Gillham <14mfg2@queensu.ca>
2018-11-11 17:47:03 -0500

    basic model

--- Deep-Q-learning/model.py
+++ Deep-Q-learning/model.py
@@ -5,8 +5,8 @@ from keras import optimizers
 def q_nn(input_size, output_size):
     model = Sequential()
     model.add(Dense(32, input_dim=input_size, activation='relu'))
-    model.add(Dense(16), activation='relu')
-    model.add(Dense(16), activation='relu')
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(16, activation='relu'))
     #softmax because actions are one-hot-encoded
     #max(model output) will gize us the best guess
     model.add(Dense(output_size, activation='softmax'))
