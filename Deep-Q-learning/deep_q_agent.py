9f2c4c756a1a6352f6e9ae6a037894718629e0e2
maxgillham <14mfg2@queensu.ca>
2018-11-11 19:25:52 -0500

    batch size 32

--- Deep-Q-learning/deep_q_agent.py
+++ Deep-Q-learning/deep_q_agent.py
@@ -2,8 +2,8 @@
 This part of code is the Q learning brain, which is a brain of the agent.
 All decisions are made in here.
 
-Difference for Deep Q is we use a nueral network to approximate our q function 
-instead of using a table.  
+Difference for Deep Q is we use a nueral network to approximate our q function
+instead of using a table.
 """
 
 import numpy as np
@@ -42,35 +42,35 @@ class deepQ:
 
     def learn(self):
         #train network on batches
-        batch_size = 2
+        batch_size = 32
         #randomly choose 32 samples from previously seen data
         batch = random.sample(self.memory, batch_size)
-        
 
-        s = np.array([m[0] for m in batch]) 
+
+        s = np.array([m[0] for m in batch])
         a = np.array([m[1] for m in batch])
-        r = np.array([m[2] for m in batch])   
+        r = np.array([m[2] for m in batch])
         s_ = np.array([m[3] for m in batch])
         d = np.array([m[4] for m in batch])
 
 
-        #Q(s_,a)    
+        #Q(s_,a)
         target = r + self.gamma * np.argmax(self.model.predict(s_), axis=1)
         target[d] = r[d]
         #Q(s,a)
         target_ = self.model.predict(s)
-        
+
         target_[range(batch_size), a] = target
 
         self.model.fit(s, target_, epochs=1, verbose=0)
 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
-    
+
     #default name to override everytime
     def save_model(self):
         self.model.save_weights('train.h5')
-    
+
     def load_model(self):
         self.model.load_weights('train.h5')
 
