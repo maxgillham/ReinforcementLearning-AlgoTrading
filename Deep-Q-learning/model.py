77dba6ab17a4799ca6fbd4b5d17ef930a6a063bc
Max Gillham <14mfg2@queensu.ca>
2018-11-16 17:41:25 -0500

    i think activations should be tanh

--- Deep-Q-learning/model.py
+++ Deep-Q-learning/model.py
@@ -1,14 +1,14 @@
 from keras.models import Sequential
 from keras.layers import Dense
-from keras import optimizers
+from keras import optimizers, activations
 
 def q_nn(input_size, output_size):
     model = Sequential()
-    model.add(Dense(32, input_dim=input_size, activation='relu'))
-    model.add(Dense(16, activation='relu'))
-    model.add(Dense(16, activation='relu'))
+    model.add(Dense(32, input_dim=input_size, activation='elu'))
+    model.add(Dense(16, activation='elu'))
+    model.add(Dense(16, activation='elu'))
     #softmax because actions are one-hot-encoded
     #max(model output) will gize us the best guess
-    model.add(Dense(output_size, activation='softmax'))
-    model.compile(loss='binary_crossentropy', optimizer=optimizers.Adam())
+    model.add(Dense(output_size, activation='linear'))
+    model.compile(loss='mse', optimizer=optimizers.Adam())
     return model
