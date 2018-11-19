4080dd7ac054bf922d5ca528d79ce7a712f4bbf1
maxgillham <14mfg2@queensu.ca>
2018-11-19 17:01:56 -0500

    added method to create iid data

--- Q-learning/utils.py
+++ Q-learning/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 
 #main method to get the return rates for the three given stocks over time
@@ -25,7 +26,9 @@ def sort_by_recent(df):
 
 #divide data into training and testing data
 def split_data(df):
-    return df.iloc[0:df.shape[0]-100], df.iloc[df.shape[0]-100:]
+    train_data = df.iloc[0:df.shape[0]-100]
+    test_data = df.iloc[df.shape[0]-100:]
+    return train_data, test_data
 
 #convert to rate of return
 def compute_return_rates(df):
@@ -38,15 +41,44 @@ def round_return_rate(df):
 #will return max and min for when defining space ranges
 def get_max_and_min(df):
     return df.values.max(), df.values.min()
-
+#makes numers like 5, 10, 15.... when given 5
 def round_to_base(value, base):
     return int(base * round(value/base))
 
+#create meta data that is i.i.d
+def create_iid(days):
+
+    #init stock lists
+    stock_1 = []
+    stock_2 = []
+    stock_3 = []
+    dummy = []
+
+    #randomly choose for each day
+    for _ in range(days):
+        stock_1.append(np.random.choice([-.1, 0, .1]))
+        stock_2.append(np.random.choice([-0.25, 0, .25]))
+        stock_3.append(np.random.choice([-0.05, 0, 0.05]))
+        dummy.append(0.0)
+
+
+    #make into pandas obj
+    data = pd.DataFrame(
+        {'stock_1': stock_1,
+         'stock_2': stock_2,
+         'stock_3': stock_3,
+         'dummy': dummy
+        })
+
+    return data
+
 if __name__ == '__main__':
     #example on how you can call it
-    return_rates = get_data()
-    print('Data Shape: ', return_rates.shape)
-    print('Column Headers are: ', list(return_rates))
-    print('First 10 return rates for IBM: \n', return_rates['ibm'].iloc[0:11])
-    return_rates_train, return_rates_test = split_data(return_rates)
-    print('Training Shape: ', return_rates_train.shape, '\nTesting Shape: ', return_rates_test.shape)
+    #return_rates = get_data()
+    #print('Data Shape: ', return_rates.shape)
+    #print('Column Headers are: ', list(return_rates))
+    #print('First 10 return rates for IBM: \n', return_rates['ibm'].iloc[0:11])
+    #return_rates_train, return_rates_test = split_data(return_rates)
+    #print('Training Shape: ', return_rates_train.shape, '\nTesting Shape: ', return_rates_test.shape)
+
+    create_iid(15)
