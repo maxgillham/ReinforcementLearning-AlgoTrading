4227e5be126b5f35b0a57198e7ccfa31bc29533a
maxgillham <maxfgillham@gmail.com>
2019-02-20 11:49:21 -0500

    declare obs space

--- Q-learning/Q_table.py
+++ Q-learning/Q_table.py
@@ -10,33 +10,45 @@ from numba import jit
 
 
 class QLearningTable:
-    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.05):
+    def __init__(self, actions, observations):
         self.actions = actions  # a list
-        self.lr = learning_rate
-        self.gamma = reward_decay
-        self.epsilon = e_greedy
+        self.lr = 0.01
+        self.gamma = 0.95
+        self.epsilon = 1
+        self.epsilon_decay = 0.9999
+        self.epsilon_min = 0.01
         self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+        self.observations = observations
+
+    def setup_table(self):
+        for obs in self.observations:
+            self.q_table = self.q_table.append(
+                pd.Series(
+                [0]*len(self.actions),
+                index=self.q_table.columns,
+                name=str(list(obs))
+                )
+            )
+        return
+
 
     @jit
     def choose_action(self, observation):
-        self.check_state_exist(observation)
-        # action selection
-        if np.random.uniform() > self.epsilon:
-            # choose best action
-            state_action = self.q_table.loc[str(observation), :]
-            # some actions may have the same value, randomly choose on in these actions
-            action = np.random.choice(state_action[state_action == np.max(state_action)].index)
-        else:
-            # choose random action
-            action = np.random.choice(self.actions)
+        #self.check_state_exist(observation)
+        # choose "current best" action
+        if np.random.rand() > self.epsilon: action = self.q_table.loc[str(observation)].idxmax()
+        # choose random action
+        else: action = np.random.choice(self.actions)
+        #decay exploration rate if not below min exploration rate
+        if self.epsilon > self.epsilon_min: self.epsilon *= self.epsilon_decay
         return action
 
     @jit
     def learn(self, s, a, r, s_):
-        self.check_state_exist(s_)
+        #self.check_state_exist(s_)
         q_predict = self.q_table.loc[s, a]
-        q_target = r + (self.gamma*self.q_table.loc[s_, :].max())
-        self.q_table.loc[s, a] += round(self.lr * (q_target - q_predict), 3)  # update
+        q_target = r + (self.gamma*self.q_table.loc[s_].max())
+        self.q_table.loc[s, a] += self.lr * (q_target - q_predict)  # update
 
     @jit
     def check_state_exist(self, state):
